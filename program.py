f1a842b929893d6eb324665277a926e6ff3f338d
megurt <mega.arte.99@gmail.com>
2020-11-14 18:39:36 +0300

    Update program.py

--- program.py
+++ program.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello 1'
+    return 'Base action'
 
 @app.route('/v2')
 def v2():
-    return 'Hello 2'
+    return 'Second action'
 
 @app.route('/Abramov')
 def v2():
