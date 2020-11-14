f9d1de4633fff5e9834ace9810d2e356a476385b
megurt <mega.arte.99@gmail.com>
2020-11-14 18:38:19 +0300

    Update program.py

--- program.py
+++ program.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from CI with GitHub Actions by Абрамов'
+    return 'Hello 1'
+
+@app.route('/v2')
+def v2():
+    return 'Hello 2'
 
 @app.route('/Abramov')
 def v2():
